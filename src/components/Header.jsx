10860765acd831b26490ab69b6bfd0358faaed44
XpuHu <ecaterinacovali93@gmail.com>
2020-10-31 17:25:00 +0300

    init commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return(
+        <header className="header">
+        {/* Logo */}
+        <img
+          src="https://i.pinimg.com/originals/d0/54/89/d054890aa6a20fe5273d24feff7acc79.jpg"
+          alt=""
+        />
+      </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" />
+      </div>
+      <div>ava + description</div>
+      <div>
+        My post
+        <div>New Post</div>
+        <div>Post 1</div>
+        <div>Post</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
