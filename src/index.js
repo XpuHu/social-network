ed18e84f00567ff8e71a876809f3e252dd179528
XpuHu <ecaterinacovali93@gmail.com>
2022-02-16 12:01:44 +0300

    rerender fix

--- src/index.js
+++ src/index.js
@@ -5,20 +5,12 @@ import store from './redux/redux-store';
 import App from './App';
 import { Provider } from "react-redux";
 
-const rerender = (store) => {
-   ReactDOM.render(
-      <Provider store={ store }>
-         <App />
-      </Provider>,
-      document.getElementById('root')
-   );
-};
-
-rerender(store);
-
-store.subscribe(() => {
-   rerender(store);
-});
+ReactDOM.render(
+   <Provider store={ store }>
+      <App />
+   </Provider>,
+   document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
